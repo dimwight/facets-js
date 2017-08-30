527d4f459a4c63525f5612be3e7e1331c289b937
dimwight <dimwight@ntlworld.com>
2017-08-30 20:33:44 +0100

    tool/delLibs.ts

--- tool/delLibs.ts
+++ tool/delLibs.ts
@@ -3,11 +3,11 @@ import * as fs from 'fs';
 const gone=[],missing=[];
 ['in/fjs/globals/Facets.js','public/Facets.js','node_modules/Facets.js']
   .forEach((path)=>{
-  if(fs.existsSync(path)){
-    gone.push(path);
-    fs.unlinkSync(path);
-  }
-  else missing.push(path);
+    if(fs.existsSync(path)){
+      gone.push(path);
+      fs.unlinkSync(path);
+    }
+    else missing.push(path);
   });
 gone.forEach((path)=>console.log('gone path='+path));
 missing.forEach((path)=>console.log('missing path='+path));
