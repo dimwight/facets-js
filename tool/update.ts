7a0481aaf45c69d146cb3c5e4eefffcec9825331
dimwight <dimwight@ntlworld.com>
2017-09-26 20:24:41 +0100

    update.ts

--- tool/update.ts
+++ tool/update.ts
@@ -1,7 +1,7 @@
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
 
 const workpath='C:/Users/Me/ts/',
-  fromPath=workpath+'facets.js/',
+  fromPath=workpath+'facets-js/',
   toPath=workpath+'react-rollup/',
   lib='public/Facets.js',
   dts='node_modules/@types/facets-js/'+'index.d.ts',
@@ -9,4 +9,14 @@ const workpath='C:/Users/Me/ts/',
   fromDts=fromPath+dts,toDts=toPath+dts,
   fromStore=fromPath+'index.d.ts',toStore=toPath+'index.d.ts';
 
-
+[[fromLib,toLib],
+  [fromDts,toDts],
+  [fromStore,toStore]
+].forEach((pair)=>{
+  fs.copySync(pair[0],pair[1]);
+  pair.forEach((path)=>{
+    const stats=fs.statSync(path);
+    const date=new Date(stats.mtime);
+    console.log(path,'\t\t', date, '\t',stats.size)
+  })
+})
