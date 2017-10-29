16ca9d8dce44e7307892ea2fc1619b6798c58a7f
dimwight <dimwight@ntlworld.com>
2017-10-29 10:45:53 +0000

    no rr*

--- tool/update.ts
+++ tool/update.ts
@@ -2,19 +2,15 @@ import * as fs from 'fs-extra';
 
 const basePath='C:/Users/Me/ts/',
   fjsRoot=basePath+'facets-js/',
-  rrRoot=basePath+'react-rollup/',
   frRoot=basePath+'facets-react/',
   lib='public/Facets.js',
   dTs='index.d.ts',
   modules='node_modules/@types/facets-js/',
   fjsLib=fjsRoot+lib,
-  rrLib=rrRoot+lib,
   frLib=frRoot+lib,
   fjsModules=fjsRoot+modules+dTs,
-  rrModules=rrRoot+modules+dTs,
   frModules=frRoot+modules+dTs,
   fjsTop=fjsRoot+dTs,
-  rrTop=rrRoot+dTs,
   frTop=frRoot+dTs;
 
 const doCopy=function(src,dest){
@@ -22,6 +18,6 @@ const doCopy=function(src,dest){
   const stats=fs.statSync(dest);
   console.log(`${dest}=${stats.size}`)
 };
-[rrLib,frLib].forEach(dest=>doCopy(fjsLib,dest));
-[fjsModules,rrModules,rrTop,frModules,frTop].forEach(
+[frLib].forEach(dest=>doCopy(fjsLib,dest));
+[fjsModules,frModules,frTop].forEach(
   (dest)=>doCopy(fjsTop,dest));
