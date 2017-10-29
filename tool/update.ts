69b5b7dcffea1a68c81afe6fcff98217cdeb0750
dimwight <dimwight@ntlworld.com>
2017-10-29 07:39:20 +0000

    update, supplement

--- tool/update.ts
+++ tool/update.ts
@@ -1,24 +1,30 @@
 import * as fs from 'fs-extra';
 
 const basePath='C:/Users/Me/ts/',
-  fromRoot=basePath+'facets-js/',
-  toRoot=basePath+'facets-react/',
+  fjsRoot=basePath+'facets-js/',
+  rrRoot=basePath+'facets-react/',
+  frRoot=basePath+'facets-react/',
   lib='public/Facets.js',
   dTs='index.d.ts',
   modules='node_modules/@types/facets-js/',
-  fromLib=fromRoot+lib,toLib=toRoot+lib,
-  fromModules=fromRoot+modules+dTs,toModules=toRoot+modules+dTs,
-  fromTop=fromRoot+dTs,toTop=toRoot+dTs;
+  fjsLib=fjsRoot+lib,
+  rrLib=frRoot+lib,
+  frLib=frRoot+lib,
+  fjsModules=fjsRoot+modules+dTs,
+  rrModules=frRoot+modules+dTs,
+  frModules=frRoot+modules+dTs,
+  rrTop=fjsRoot+dTs,
+  fjsTop=fjsRoot+dTs,
+  frTop=frRoot+dTs;
 
 const doCopy=function(src,dest){
   fs.copySync(src,dest);
   const stats=fs.statSync(dest);
   console.log(`${dest}=${stats.size}`)
 };
-if(true)doCopy(fromLib,toLib);
-const master=fromTop;
-(master===fromTop?[fromModules,toModules,toTop]
-:master===toModules?[fromModules,fromTop,toTop]
-:[]
-).forEach(
+if(true)doCopy(fjsLib,frLib);
+const master=fjsTop;
+(master===fjsTop?[fjsModules,rrModules,rrTop,frModules,frTop]
+:master===frModules?[fjsModules,rrModules,rrTop,frModules,frTop]
+:null).forEach(
   (dest)=>doCopy(master,dest));
